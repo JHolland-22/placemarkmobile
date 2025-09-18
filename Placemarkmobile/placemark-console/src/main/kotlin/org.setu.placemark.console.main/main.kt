851e10a55fb31d750c004acb1620117bb8c4c2fe
JHolland <20102340@mail.wit.ie>
2025-09-18 19:55:02 +0100

    Menu function

--- Placemarkmobile/placemark-console/src/main/kotlin/org.setu.placemark.console.main/main.kt
+++ Placemarkmobile/placemark-console/src/main/kotlin/org.setu.placemark.console.main/main.kt
@@ -3,8 +3,35 @@ import mu.KotlinLogging
 
 private val logger = KotlinLogging.logger {}
 
-fun main(){
-    println("Placemark Kotlin App Version 1.0")
+fun main() {
     logger.info { "Launching Placemark Console App" }
+    println("Placemark Kotlin App Version 1.0")
+
+    var input: Int
+
+    input = menu()
 
 }
+
+
+
+fun menu() : Int {
+
+    var option : Int
+    var input: String? = null
+
+    println("Main Menu")
+    println(" 1. Add Placemark")
+    println(" 2. Update Placemark")
+    println(" 3. List All Placemarks")
+    println("-1. Exit")
+    println()
+    print("Enter an integer : ")
+    input = readLine()!!
+    option = if (input.toIntOrNull() != null && !input.isEmpty())
+        input.toInt()
+    else
+        -9
+    return option
+}
+
