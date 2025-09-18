9dbb517cfc79bdfc27a79cbf559029b8dd5f584d
JHolland <20102340@mail.wit.ie>
2025-09-18 19:43:13 +0100

    added logging support to Kotlin project

--- Placemarkmobile/placemark-console/src/main/kotlin/org.setu.placemark.console.main/main.kt
+++ Placemarkmobile/placemark-console/src/main/kotlin/org.setu.placemark.console.main/main.kt
@@ -1,5 +1,10 @@
 #!/usr/bin/env kotlin
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
 
 fun main(){
     println("Placemark Kotlin App Version 1.0")
+    logger.info { "Launching Placemark Console App" }
+
 }
